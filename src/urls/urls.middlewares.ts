6553cef66f5afe4d8a06c68ac928a38e26dabf56
FernandoLorca <florcapinto@gmail.com>
2024-01-31 10:51:28 -0300

    Create function to validate token from bearer

--- src/urls/urls.middlewares.ts
+++ src/urls/urls.middlewares.ts
@@ -1,8 +1,52 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+
+interface CustomRequest extends Request {
+  token: string | JwtPayload;
+}
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.headers.authorization);
+  const bearerHeader = req.headers.authorization;
+
+  if (!bearerHeader) {
+    return res.status(401).json({
+      ok: false,
+      status: 401,
+      message: 'Token is required',
+    });
+  }
+
+  try {
+    const token = bearerHeader.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as Secret, {
+      ignoreExpiration: true,
+    });
+
+    if (decoded && typeof decoded === 'object' && decoded.exp !== undefined) {
+      const expirationTimestamp = decoded.exp * 1000;
+      const currentTimestamp = Date.now();
+
+      if (expirationTimestamp < currentTimestamp) {
+        return res.status(401).json({
+          ok: false,
+          status: 401,
+          message: 'Token expired',
+        });
+      }
+    }
+    (req as CustomRequest).token = decoded;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      return res.status(401).json({
+        ok: false,
+        status: 401,
+        message: `Internal server error: ${error.message}`,
+      });
+    }
+  }
 };
 
 export const urlsMiddlewares = {
