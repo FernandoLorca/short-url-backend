64373c771bd77d8a875478f9ff273efdc10cc254
FernandoLorca <florcapinto@gmail.com>
2024-02-05 12:19:35 -0300

    Test for token verification, existance and expiration

---
+++ src/urls/urls.middlewares.test.ts
@@ -0,0 +1,62 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { Request, Response, NextFunction } from 'express';
+import { urlsMiddlewares } from './urls.middlewares';
+
+interface CustomRequest extends Request {
+  headers: {
+    authorization: string;
+  };
+}
+
+const mockRequest = {
+  headers: {
+    authorization: '',
+  },
+} as CustomRequest;
+
+const mockResponse: Response = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+} as any;
+
+const mockNextFn: NextFunction = jest.fn();
+
+describe('verifyToken', () => {
+  beforeEach(() => {
+    process.env.JWT_SECRET;
+  });
+
+  it("should return 401 if token dosen't exist", () => {
+    mockRequest.headers.authorization = '';
+
+    urlsMiddlewares.verifyToken(mockRequest, mockResponse, mockNextFn);
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      ok: false,
+      status: 401,
+      message: 'Token is required',
+    });
+  });
+
+  it('Should return 401 if token is expired', () => {
+    mockRequest.headers.authorization =
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsImVtYWlsIjoiZmVybmFuZG9sb3JjYUBnbWFpbC5jb20iLCJpYXQiOjE3MDY3Mjc2NjAsImV4cCI6MTcwNjgxNDA2MH0.jumm1djyRvE4sLkg33zFTQB51DoHTV4-2KUzdaav94I';
+
+    urlsMiddlewares.verifyToken(mockRequest, mockResponse, mockNextFn);
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      ok: false,
+      status: 401,
+      message: 'Token expired',
+    });
+  });
+
+  // it('Should return 500 Internal error for token malformed', () => {
+  //   mockRequest.headers.authorization =
+  //     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsImVtYWlsIjoiZmVybmFuZG9sb3JjYUBnbWFpbC5jb20iLCJpYXQiOjE3MDY3Mjc2NjAsImV4cCI6MTcwNjgxNDA2MH0.jumm1djyRvE4sLkg33zFTQB51DoHTV4-2KUzdaav94';
+  // });
+
+  // urlsMiddlewares.verifyToken(mockRequest, mockResponse, mockNextFn);
+  // expect(mockResponse.status).toHaveBeenCalledWith(500)
+});
