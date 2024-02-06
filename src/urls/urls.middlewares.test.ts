aee04226248d0ad79b03f00aa19542a2966a8078
FernandoLorca <florcapinto@gmail.com>
2024-02-06 09:56:03 -0300

    Test for refreshToken fn

--- src/urls/urls.middlewares.test.ts
+++ src/urls/urls.middlewares.test.ts
@@ -9,6 +9,19 @@ interface CustomRequest extends Request {
   };
 }
 
+interface RequestWithToken extends CustomRequest {
+  token: {
+    decoded: {
+      id: number;
+      email: string;
+      iat: number;
+      exp: number;
+    };
+    token: string;
+    tokenExpired: boolean;
+  };
+}
+
 const mockRequest = {
   headers: {
     authorization: '',
@@ -21,6 +34,14 @@ const mockResponse: Response = {
 } as any;
 
 const mockNextFn: NextFunction = jest.fn();
+
+const mockTokenRequest = {
+  token: {
+    decoded: {},
+    token: '',
+    tokenExpired: false,
+  },
+} as RequestWithToken;
 
 describe('verifyToken', () => {
   beforeEach(() => {
@@ -83,6 +104,52 @@ describe('verifyToken', () => {
       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsImVtYWlsIjoiZmVybmFuZG9sb3JjYUBnbWFpbC5jb20iLCJpYXQiOjE3MDY3Mjc2NjAsImV4cCI6MTcwNjgxNDA2MH0.jumm1djyRvE4sLkg33zFTQB51DoHTV4-2KUzdaav94I';
 
     urlsMiddlewares.verifyToken(mockRequest, mockResponse, mockNextFn);
+    expect(mockNextFn).toHaveBeenCalled();
+  });
+});
+
+describe('refreshToken', () => {
+  it('Should return 404 for user not found', async () => {
+    mockTokenRequest.token.decoded = {
+      id: 9999,
+      email: 'fernandolorca@gmail.com',
+      iat: 1706727660,
+      exp: 1706814060,
+    };
+    mockTokenRequest.token.token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsImVtYWlsIjoiZmVybmFuZG9sb3JjYUBnbWFpbC5jb20iLCJpYXQiOjE3MDcyMjEzOTgsImV4cCI6MTcwNzMwNzc5OH0.QULNBpRHC6S43CYMx3Wqwf-REayx1AkLWS7-00B1Rls';
+    mockTokenRequest.token.tokenExpired = false;
+
+    await urlsMiddlewares.refreshToken(
+      mockTokenRequest,
+      mockResponse,
+      mockNextFn
+    );
+    expect(mockResponse.status).toHaveBeenCalledWith(404);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      ok: false,
+      status: 404,
+      message: 'User not found',
+    });
+  });
+
+  it('Should refresh token and call next function', async () => {
+    mockTokenRequest.token.decoded = {
+      id: 11,
+      email: 'fernandolorca@gmail.com',
+      iat: 1706727660,
+      exp: 1706814060,
+    };
+    mockTokenRequest.token.token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsImVtYWlsIjoiZmVybmFuZG9sb3JjYUBnbWFpbC5jb20iLCJpYXQiOjE3MDcyMjEzOTgsImV4cCI6MTcwNzMwNzc5OH0.QULNBpRHC6S43CYMx3Wqwf-REayx1AkLWS7-00B1Rls';
+    mockTokenRequest.token.tokenExpired = false;
+
+    await urlsMiddlewares.refreshToken(
+      mockTokenRequest,
+      mockResponse,
+      mockNextFn
+    );
+
     expect(mockNextFn).toHaveBeenCalled();
   });
 });
