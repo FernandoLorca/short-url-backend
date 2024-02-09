e6944eb6966ea89b7c66598bad8103ce74097d63
FernandoLorca <florcapinto@gmail.com>
2024-02-09 09:53:46 -0300

    updateCustomLinkAdd fn ready

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -39,13 +39,88 @@ const storageUrlDatabase = async (req: Request, res: Response) => {
 
 const updateCustomLink = async (req: Request, res: Response) => {
   const id = req.user?.id;
+  const { urlLinkToUpdate, customLink } = req.body;
 
-  const urls = await Urls.findAll({
-    where: {
-      userId: id,
-    },
-  });
-  console.log(urls);
+  try {
+    const linkToUpdate = await Urls.findOne({
+      where: {
+        original: urlLinkToUpdate,
+      },
+    });
+
+    if (!linkToUpdate) {
+      res.status(404).json({
+        ok: false,
+        status: 404,
+        message: "Link dosn't exist",
+      });
+      return;
+    }
+
+    if (linkToUpdate.dataValues.userId !== id) {
+      res.status(403).json({
+        ok: false,
+        status: 403,
+        message: 'You are not allowed to update this link',
+      });
+      return;
+    }
+
+    if (linkToUpdate.dataValues.customLink === customLink) {
+      res.status(400).json({
+        ok: false,
+        status: 400,
+        message: 'Custom link already have that value',
+      });
+      return;
+    }
+
+    const updateCustomLink = await Urls.update(
+      {
+        customLink,
+      },
+      {
+        where: {
+          customLink: linkToUpdate.dataValues.customLink,
+        },
+      }
+    );
+
+    if (updateCustomLink[0] !== 1) {
+      res.status(404).json({
+        ok: false,
+        status: 404,
+        message: "Can't update the link",
+      });
+      return;
+    }
+
+    const linkWithNewCustomLink = await Urls.findOne({
+      where: {
+        original: urlLinkToUpdate,
+      },
+    });
+
+    res.status(201).json({
+      ok: true,
+      status: 201,
+      data: {
+        id: linkWithNewCustomLink?.dataValues.id,
+        originalUrl: linkWithNewCustomLink?.dataValues.original,
+        shortUrl: linkWithNewCustomLink?.dataValues.short,
+        customLink: linkWithNewCustomLink?.dataValues.customLink,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      res.status(500).json({
+        ok: false,
+        status: 500,
+        message: `Internal server error: ${error.message}`,
+      });
+    }
+  }
 };
 
 export const urlsController = {
