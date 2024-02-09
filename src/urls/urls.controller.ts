56c046147af74440b9d02d08e2f6addcd7189aa5
FernandoLorca <florcapinto@gmail.com>
2024-02-09 09:57:21 -0300

    updateCustomLinkAdd fn, add custom link verification value

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -41,6 +41,14 @@ const updateCustomLink = async (req: Request, res: Response) => {
   const id = req.user?.id;
   const { urlLinkToUpdate, customLink } = req.body;
 
+  if (!customLink) {
+    res.status(400).json({
+      ok: false,
+      status: 400,
+      message: 'Custom link must have a value',
+    });
+  }
+
   try {
     const linkToUpdate = await Urls.findOne({
       where: {
