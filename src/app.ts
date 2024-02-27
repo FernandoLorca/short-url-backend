19cc6b42b6fd4ae0260fb1fbc346dbcd1f72d6ed
FernandoLorca <florcapinto@gmail.com>
2024-02-27 11:13:54 -0300

    Install swagger

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express, { Application } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import swaggerUI from 'swagger-ui-express';
 import usersRouter from './users/users.routes';
 import urlsRouter from './urls/urls.routes';
 
@@ -11,6 +12,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(`${process.env.API_VERSION}/docs`, swaggerUI.serve);
 app.use(`${process.env.API_VERSION}/user`, usersRouter);
 app.use(`${process.env.API_VERSION}/urls`, urlsRouter);
 
